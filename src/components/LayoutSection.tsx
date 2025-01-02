7791e848a7b0e3888a26ce5961a1561fd770896d
Haldies Gerhardien Pasya <haldiespasya@gmail.com>
2025-01-03 06:22:04 +0700

    feat: add LayoutSection component with SectionTitle integration

---
+++ src/components/LayoutSection.tsx
@@ -0,0 +1,11 @@
+import SectionTitle from "./SectionTitle"
+
+function LayoutSection() {
+  return (
+    <div className="p-6  shadow-lg rounded-lg mt-6 w-full max-h-96 h-full max-w-96 border-2 border-gray-700">
+    <SectionTitle title="Berkas Wajib" description="Siapkan dari awal karena sulit mendapatkannya" />
+    </div>
+  )
+}
+
+export default LayoutSection
